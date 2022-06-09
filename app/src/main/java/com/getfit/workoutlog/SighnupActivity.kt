976b86337755054b32a06f9adbb968825c7e1044
hilda1737 <hildamoraahh@gmail.com>
2022-06-09 17:56:38 +0300

    added login activity

--- app/src/main/java/com/getfit/workoutlog/SighnupActivity.kt
+++ app/src/main/java/com/getfit/workoutlog/SighnupActivity.kt
@@ -3,10 +3,13 @@ package com.getfit.workoutlog
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.ContactsContract
+import android.util.Patterns
 import android.widget.Button
 import android.widget.TextView
 import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
+import java.util.regex.Pattern
 
 class SighnupActivity : AppCompatActivity() {
     lateinit var etEmail3: TextInputEditText
@@ -59,9 +62,9 @@ fun validateSignUp() {
     var error = false
     tilEmail3.error = null
     tilpassword.error = null
-    tilFirstname.error=null
-    tilSecond.error=null
-    tilConfirm.error=null
+    tilFirstname.error = null
+    tilSecond.error = null
+    tilConfirm.error = null
     var email = etEmail3.text.toString()
     if (email.isBlank()) {
         tilEmail3.error = "Error is required "
@@ -69,6 +72,16 @@ fun validateSignUp() {
 
 
     }
+
+
+    if (!Patterns.EMAIL_ADDRESS.matcher(email).matches()) {
+        tilEmail3.error = "Not valid Email"
+        error = true
+
+
+    }
+
+
          var first=etFirstname.text.toString()
         if (first.isBlank()) {
             tilFirstname.error = "First name is required"
@@ -96,6 +109,8 @@ fun validateSignUp() {
       if (!error){
 
     }
+    if(password!=confirm)
+        tilConfirm.error="password error!"
 }
 }
 
